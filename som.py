b763280185ddb040712dc7bc7a7738526118102d
xalkan <shahidalyk@gmail.com>
2018-10-09 21:25:19 +0500

    visualizing som

--- som.py
+++ som.py
@@ -22,3 +22,32 @@ from minisom import MiniSom
 som = MiniSom(x = 10, y = 10, input_len = 15, sigma = 1.0, learning_rate = 0.5)
 som.random_weights_init(X)
 som.train_random(data = X, num_iteration = 1)
+
+# Visualizing the results
+# mid = mean inter-neuron distance between neurons
+# higher mid = outlier = encoded in color
+from pylab import bone, pcolor, colorbar, plot, show
+bone()
+pcolor(som.distance_map().T)    # T is transpose of the mid matrix
+colorbar()
+# highlight customers 
+# red - customers who didnt get approval
+# green - customers who got approval
+markers = ['o', 's']
+colors = ['r', 'g']
+
+for i, customer in enumerate(X):
+    # winning node of the customer at i
+    winning_node = som.winner(customer)
+    # plot marker at the center of the node
+    # y[customer] is the approval label, either 0 or 1
+    plot(winning_node[0] + 0.5, winning_node[1] + 0.5,
+         markers[y[i]], markeredgecolor = colors[y[i]],
+         markerfacecolor = 'None', markersize = 10, markeredgewidth = 2 )
+
+show()
+
+
+
+
+a = 1
